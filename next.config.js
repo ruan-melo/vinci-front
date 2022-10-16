81b74e0b9daf481fa541041299ade2903c2be5de
Ruan Melo <ruantmelo@gmail.com>
2022-10-16 16:11:15 -0300

    fix nextjs images

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'flowbite'],
-  }
+    domains: [
+      'localhost',
+      'flowbite',
+      'd849-2804-29b8-513a-40c0-92b7-d728-704b-68c3.sa.ngrok.io',
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/services/apolloClient.ts
+++ src/services/apolloClient.ts
@@ -1,25 +1,3 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-import { parseCookies } from 'nookies'
+import { getApolloClient } from './getApolloClient'
 
-const httpLink = createHttpLink({
-  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
-})
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const { 'vinci:access_token': access_token } = parseCookies()
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: access_token ? `Bearer ${access_token}` : '',
-    },
-  }
-})
-
-export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-  connectToDevTools: true,
-})
+export const client = getApolloClient()
